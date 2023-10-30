7e52cb524a7cc75fd69635b6c64aecc393563721
v.popov <v.popov@jetmail.cc>
2023-10-31 00:01:08 +0300

    добавил сокеты

--- server/markets/caseBattle.js
+++ server/markets/caseBattle.js
@@ -4,7 +4,7 @@ const { cases } = require("./caseBattleCases");
 let handleMessage = null
 const ignoreCases = ['upgrade']
 const ignoreId = new Set()
-let cookie = 'supportOnlineTalkID=WN0PQV0hq9g80R7SML674ziP5PCSw2QH; _sessionIdentity=1ehnt0qriarat0lr3dl1ddpjp4; _csrfParam=6127e840ff2e3c2ad32c78fa6d300b835f72e60850702f0002d6f1c3c90afa38a%3A2%3A%7Bi%3A0%3Bs%3A10%3A%22_csrfParam%22%3Bi%3A1%3Bs%3A32%3A%225ZVGPOBJ5_4Vn00UgCtNAF0jD8-bYh6y%22%3B%7D; cf_clearance=kfE3OLQbjobhbn__hcMYTLEpr90UACPg.TBbUNV5_ng-1698692187-0-1-55c801b8.940f0918.2c95e58-160.0.0'
+let cookie = 'cf_clearance=u6.suxtwoaazYYQZuir5GjJgVYAVn7jDKR5P4lxDy4o-1698699248-0-1-55c801b8.edf3c715.2c95e58-160.0.0; supportOnlineTalkID=hxh1W2zfJ1f7wk9qvCSH3VnlKQJTOUAH; _sessionIdentity=m5l1atdg6sg746of6ot7ippsk4; _csrfParam=efb17cd764468cfd0e755f66be09b0e4b0356f6f9d7cc12a96161d905e4f7756a%3A2%3A%7Bi%3A0%3Bs%3A10%3A%22_csrfParam%22%3Bi%3A1%3Bs%3A32%3A%22ma1dDwrzxcZEXZCSp5FcvF5OUsQh2O_q%22%3B%7D'
 
 let ws = new WebSocketClient('');
 let connectionWs = null
@@ -49,8 +49,14 @@ const parseData = (data) => {
 const connect = () => {
     setTimeout(() => {
         ws.connect('wss://case-battle.io/ws', 'echo-protocol', null ,{
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'ru',
+            'Connection': 'Upgrade',
+            'Host': 'case-battle.io',
+            'Origin': 'https://case-battle.io',
+            'Pragma': 'no-cache',
             'Sec-Websocket-Extensions': 'permessage-deflate; client_max_window_bits',
-            'Sec-Websocket-Key': 'BdEVb4iMaratkLiWiQXh1w==',
+            'Sec-Websocket-Key': 'iBdcSJDFGrwB8Kcdmqpyxg==',
             'Cookie': cookie,
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'
         });
